2261b11c865496802836b98cad752954dc591250
shaavan <shaavan.github@gmail.com>
2024-03-11 14:48:27 +0530

    Introduce Functions to read blinded_path, and channel_graphs.
    1. Introduce temporary blinded_path.json and channel_graph.json
    2. Mark Task-1 as completed.

--- pathbreaker/src/main.rs
+++ pathbreaker/src/main.rs
@@ -1,3 +1,38 @@
+mod utils;
+
+use utils::{read_blinded_path_from_file, read_channel_graph_from_file};
+
 fn main() {
-    println!("Hello, world!");
+    // Path to the JSON file
+    let blinded_path_file = "data/blinded_path.json";
+    let channel_graph_file = "./data/channel_graph.json";
+
+    match read_blinded_path_from_file(blinded_path_file) {
+        Ok(blinded_path) => {
+            println!("Introduction Node: {}", blinded_path.introduction_node);
+            println!("Blinded Nodes: {:?}", blinded_path.blinded_nodes);
+            println!("Fee Base (msat): {}", blinded_path.fee_base_msat);
+            println!(
+                "Fee Proportional Millionths: {}",
+                blinded_path.fee_proportional_millionths
+            );
+            println!("HTLC Minimum (msat): {}", blinded_path.htlc_minimum_msat);
+            println!("CLTV Expiry Delta: {}", blinded_path.cltv_expiry_delta);
+            println!("Max CLTV Expiry: {}", blinded_path.max_cltv_expiry);
+        }
+        Err(err) => {
+            eprintln!("Error reading file: {}", err);
+        }
+    }
+
+    match read_channel_graph_from_file(channel_graph_file) {
+        Ok(channel_graph) => {
+            // Do whatever you need with the data
+            println!("Number of nodes: {}", channel_graph.nodes.len());
+            println!("Number of edges: {}", channel_graph.edges.len());
+        }
+        Err(err) => {
+            eprintln!("Error reading file: {}", err);
+        }
+    }
 }
---
+++ pathbreaker/src/utils.rs
@@ -0,0 +1,87 @@
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+#[derive(Debug, Deserialize)]
+pub struct BlindedPath {
+    pub introduction_node: String,
+    pub blinded_nodes: Vec<String>,
+    pub fee_base_msat: u64,
+    pub fee_proportional_millionths: u64,
+    pub htlc_minimum_msat: u64,
+    pub cltv_expiry_delta: u32,
+    pub max_cltv_expiry: u32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ChannelGraph {
+    pub nodes: Vec<Node>,
+    pub edges: Vec<Edge>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Node {
+    pub last_update: i64,
+    pub pub_key: String,
+    pub alias: String,
+    pub addresses: Vec<Address>,
+    pub color: String,
+    pub features: serde_json::Value,
+    pub custom_records: serde_json::Value,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Address {
+    pub network: String,
+    pub addr: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Edge {
+    pub channel_id: String,
+    pub chan_point: String,
+    pub last_update: i64,
+    pub node1_pub: String,
+    pub node2_pub: String,
+    pub capacity: String,
+    pub node1_policy: Policy,
+    pub node2_policy: Policy,
+    pub custom_records: serde_json::Value,
+}
+#[derive(Debug, Deserialize)]
+pub struct Policy {
+    pub time_lock_delta: i64,
+    pub min_htlc: String,
+    pub fee_base_msat: String,
+    pub fee_rate_milli_msat: String,
+    pub disabled: bool,
+    pub max_htlc_msat: String,
+    pub last_update: i64,
+    pub custom_records: serde_json::Value,
+}
+
+pub fn read_channel_graph_from_file(file_path: &str) -> Result<ChannelGraph, Box<dyn Error>> {
+    let path = Path::new(file_path);
+    let mut file = File::open(&path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let channel_graph: ChannelGraph = serde_json::from_str(&contents)?;
+    Ok(channel_graph)
+}
+
+pub fn read_blinded_path_from_file(file_path: &str) -> Result<BlindedPath, Box<dyn Error>> {
+    // Open the file in read-only mode
+    let path = Path::new(file_path);
+    let mut file = File::open(&path)?;
+
+    // Read the file contents into a string
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    // Parse the JSON string into a BlindedPath object
+    let blinded_path: BlindedPath = serde_json::from_str(&contents)?;
+
+    Ok(blinded_path)
+}

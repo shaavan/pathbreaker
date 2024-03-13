d8465cb4c52ea56e86fcc1fa6025b44bdc3c30ab
shaavan <shaavan.github@gmail.com>
2024-03-13 19:19:50 +0530

    Get Probabality of a Node
    1. Introduce codes and structs to find the probability of each node
       being the receiver node.

--- pathbreaker/src/main.rs
+++ pathbreaker/src/main.rs
@@ -1,12 +1,18 @@
 mod utils;
 mod channel_route;
+mod normalise;
+
+use normalise::get_most_probable_receiver;
 use utils::read_channel_graph_from_file;
-use crate::{channel_route::{find_end_points, get_final_routes}, utils::read_blinded_path_from_file};
+use crate::{channel_route::get_final_routes, utils::read_blinded_path_from_file};
 
 fn main() {
     // Assuming channel_graph and other variables are defined
     let channel_graph = read_channel_graph_from_file("data/test_channel_graph.json").unwrap();
     let blinded_path = read_blinded_path_from_file("data/test_blinded_path.json").unwrap();
     let final_routes = get_final_routes(&channel_graph, &blinded_path);
-    println!("{:?}", find_end_points(&final_routes))
+
+    let final_probs = get_most_probable_receiver(final_routes, blinded_path);
+
+    println!("{:?}", final_probs)
 }
